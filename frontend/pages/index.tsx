17c87aa6dc591115d159a7298d57abfa32692e0d
이찬호 <chanho@woowahan.com>
2021-01-03 14:39:19 +0900

    add hello world!

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,6 +6,7 @@ const Container = styled.div`
   width: 100%;
   height: 100vh;
   justify-content: center;
+  flex-direction: column;
   align-items: center;
   .index-title {
     font-size: 24px;
@@ -18,6 +19,9 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        <h1>hello world!!!!</h1>
+      </div>
       <div className="index-title">welcome!!!</div>
     </Container>
   )
