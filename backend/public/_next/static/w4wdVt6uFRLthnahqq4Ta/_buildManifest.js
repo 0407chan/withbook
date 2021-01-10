77c57bdf1fa516d363f393dc18ef48245f63ad89
이찬호 <chanho@woowahan.com>
2021-01-10 23:20:09 +0900

    리빌드 고우

---
+++ backend/public/_next/static/w4wdVt6uFRLthnahqq4Ta/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0204b2709c22c21c3c40.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b32bd5bcb2ba9cc2dbe8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
