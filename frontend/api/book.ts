7d862de72a7845059fcfd7744efc8c45293cbb7e
이찬호 <chanho@woowahan.com>
2021-01-10 23:07:00 +0900

    api 요청을 aws-server로 변경

--- frontend/api/book.ts
+++ frontend/api/book.ts
@@ -1,24 +1,18 @@
 import axios from 'axios'
 import { Book, BookAddType } from '../types'
 
+const API_SERVER = 'https://withbook.ml'
 export const fetchAllBooks = async () => {
-  console.log('책 가져오기', window.location.href)
-  const res = await axios.get<Book[]>('http://localhost:3010/api/v1/book')
+  const res = await axios.get<Book[]>(`${API_SERVER}/api/v1/book`)
   return res.data
 }
 
 export const addBook = async (newBook: BookAddType) => {
-  console.log('책 추가하기', window.location.href)
-  const res = await axios.post<Book>(
-    'http://localhost:3010/api/v1/book',
-    newBook
-  )
+  const res = await axios.post<Book>(`${API_SERVER}/api/v1/book`, newBook)
   return res.data
 }
 export const deleteBook = async (id: number) => {
-  const res = await axios.delete<number>(
-    `http://localhost:3010/api/v1/book?id=${id}`
-  )
+  const res = await axios.delete<number>(`${API_SERVER}/api/v1/book?id=${id}`)
 
   return res.data
 }
