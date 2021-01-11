44b17d16bbfd0cb4a97d32924dbdd71a000a6aa8
이찬호 <chanho@woowahan.com>
2021-01-11 23:31:47 +0900

    book type 추가 및 변경된 타입 적용

--- frontend/api/book.ts
+++ frontend/api/book.ts
@@ -1,14 +1,14 @@
 import axios from 'axios'
-import { Book, BookAddType } from '../types'
+import { BookType, BookAddType } from '../types'
 
 const API_SERVER = 'https://withbook.ml'
 export const fetchAllBooks = async () => {
-  const res = await axios.get<Book[]>(`${API_SERVER}/api/v1/book`)
+  const res = await axios.get<BookType[]>(`${API_SERVER}/api/v1/book`)
   return res.data
 }
 
 export const addBook = async (newBook: BookAddType) => {
-  const res = await axios.post<Book>(`${API_SERVER}/api/v1/book`, newBook)
+  const res = await axios.post<BookType>(`${API_SERVER}/api/v1/book`, newBook)
   return res.data
 }
 export const deleteBook = async (id: number) => {
--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -1,8 +1,30 @@
-export type Book = {
+export type BookType = {
   id: number
   title: string
+  createdAt: string
+  updatedAt: string
+  image?: string
+  userId: number
+  bookmarkList?: BookMark
 }
 
+export type BookMark = {
+  id: number
+  bookId: number
+  title: string
+  bookPage: number
+  createdAt: string
+  updatedAt: string
+  commentList?: Comment
+}
+
+export type Comment = {
+  id: number
+  content: string
+  createdAt: string
+  updatedAt: string
+  writer: string
+}
 export type BookAddType = {
   title: string
 }
