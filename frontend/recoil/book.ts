7b83a5cf9208ec9015940b125b886c8f1c93ca99
이찬호 <0407chan@naver.com>
2021-01-16 01:18:22 +0900

    chore. book 기본 더비 데이터 삭제

--- frontend/recoil/book.ts
+++ frontend/recoil/book.ts
@@ -1,7 +1,6 @@
 import { atom, selector } from 'recoil'
 import { BookType } from '../types'
-import { DUMMY_BOOK } from '../config'
 export const bookListState = atom<BookType[]>({
   key: 'bookListState',
-  default: DUMMY_BOOK
+  default: []
 })
