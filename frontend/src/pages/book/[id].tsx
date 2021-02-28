b00617834114dcf08e0ba47c28e251436451702b
이찬호 <0407chan@naver.com>
2021-02-28 23:58:42 +0900

    feature. 책방에 북마크 섹션 추가

--- frontend/src/pages/book/[id].tsx
+++ frontend/src/pages/book/[id].tsx
@@ -1,15 +1,20 @@
 import { Space } from 'antd'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import styled from 'styled-components'
 import API from '../../api'
+import { fetchBook } from '../../api/book'
 import { addBookmark } from '../../api/bookmark'
 import BookMark from '../../components/BookMark'
 import { Maybe } from '../../components/utils/Maybe'
 import { DAY_BG_COLOR, NIGHT_BG_COLOR } from '../../config/day-night-mode'
-import { bookMarkListState } from '../../recoil/book'
+import {
+  bookMarkListState,
+  currentBookMarkState,
+  currentBookState
+} from '../../recoil/book'
 import { isDayState } from '../../recoil/day-night'
 import type { BookType } from '../../types'
 import { BookmarkBodyType, BookmarkType } from '../../types/bookmark'
@@ -21,68 +26,111 @@ const Container = styled.div<ContainerProps>`
   display: flex;
   width: 100%;
   height: 100%;
-  overflow-y: scroll;
+
   transition: background-color 200ms ease;
   background-color: ${(props) => (props.isDay ? DAY_BG_COLOR : NIGHT_BG_COLOR)};
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
 
-  ::-webkit-scrollbar {
-    display: none;
-  }
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-
-  .book-info {
+  .book-wrapper {
     display: flex;
-  }
-  .book-add-container {
-    input {
-      margin-right: 20px;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    height: 100%;
+    width: 100;
+    flex: 1;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    .book-add-container {
+      input {
+        margin-right: 20px;
+      }
+    }
+
+    .bookmark-add-button {
+      display: flex;
+      margin: 20px;
+      background-color: #fff;
+      width: 50px;
+      height: 50px;
+      cursor: pointer;
+      justify-content: center;
+      align-items: center;
+
+      transition: filter 200ms ease;
+      &:hover {
+        filter: brightness(0.7);
+      }
     }
   }
 
-  .bookmark-add-button {
+  .bookmark-wrapper {
     display: flex;
-    margin: 20px;
-    background-color: #fff;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    justify-content: center;
-    align-items: center;
-
-    transition: filter 200ms ease;
-    &:hover {
-      filter: brightness(0.7);
+    flex: 0 auto;
+    height: 100%;
+    width: 300px;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+      display: none;
     }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    border-left: 1px solid #393652;
   }
 `
 
-type BookRoomProps = {
-  book: BookType
-  bookmarkList: BookmarkType[]
-}
-const BookRoom: NextPage<BookRoomProps> = ({ book, bookmarkList }) => {
+type BookRoomProps = {}
+const BookRoom: NextPage<BookRoomProps> = ({}) => {
   const isDay = useRecoilValue(isDayState)
   const [bookmarks, setBookmarks] = useRecoilState<BookmarkType[]>(
     bookMarkListState
   )
+  const [currentBookMark, setCurrentBookMark] = useRecoilState(
+    currentBookMarkState
+  )
+  const [currentBook, setCurrentBook] = useRecoilState<BookType>(
+    currentBookState
+  )
+
+  const initBook = useRef(() => {})
+  initBook.current = async () => {
+    let bookId = undefined
+    if (currentBook === undefined) {
+      const path = window.location.pathname.split('/')
+      bookId = path[path.length - 1]
+    } else {
+      bookId = currentBook.id
+    }
+
+    const bookPayload = await fetchBook(Number(bookId))
+    setCurrentBook(bookPayload[0])
+    const bookMarkPayload = await API.Bookmark.fetchAllBookmarks(Number(bookId))
+    setBookmarks(bookMarkPayload)
+  }
   useEffect(() => {
-    setBookmarks(bookmarkList)
+    initBook.current()
   }, [])
+
+  useEffect(() => {
+    console.log(currentBookMark)
+  }, [currentBookMark])
 
   const router = useRouter()
 
   const deleteBookAction = async () => {
-    const res = await API.Book.deleteBook(Number(book.id))
+    const res = await API.Book.deleteBook(Number(currentBook.id))
     router.push('/')
   }
 
   const bookmarkAdd = async () => {
     const randomNumber = Math.floor(Math.random() * 10) + 1
     const params: BookmarkBodyType = {
-      bookId: Number(book.id),
+      bookId: Number(currentBook.id),
       bookpage: randomNumber,
       title: '헤헤헿ㅎ'
     }
@@ -93,47 +141,33 @@ const BookRoom: NextPage<BookRoomProps> = ({ book, bookmarkList }) => {
 
   return (
     <Container isDay={isDay}>
-      <div className="book-info">
+      <div className="book-wrapper">
         <div>
-          <h1>{book.title}</h1>
+          <h1>{currentBook?.title}</h1>
         </div>
-        <div className="book-updatedAd">{book.updatedAt}</div>
-        <div className="book-userId">{book.userId}</div>
+        <div className="book-updatedAd">{currentBook?.updatedAt}</div>
+        <div className="book-userId">{currentBook?.userId}</div>
+        <Maybe is={bookmarks.length > 0}>
+          <Space
+            wrap
+            direction="horizontal"
+            size={20}
+            style={{ justifyContent: 'center' }}
+          >
+            {bookmarks.map((bookmark) => (
+              <BookMark key={bookmark.id} bookmark={bookmark}></BookMark>
+            ))}
+          </Space>
+        </Maybe>
 
-        {/* <button
-          className="book-delete-button"
-          onClick={() => deleteBookAction()}
-        >
-          X
-        </button> */}
+        <div className="bookmark-add-button" onClick={() => bookmarkAdd()}>
+          +
+        </div>
       </div>
-      <Maybe is={bookmarks.length > 0}>
-        <Space
-          wrap
-          direction="horizontal"
-          size={20}
-          style={{ justifyContent: 'center' }}
-        >
-          {bookmarks.map((bookmark) => (
-            <BookMark key={bookmark.id} bookmark={bookmark}></BookMark>
-          ))}
-        </Space>
-      </Maybe>
-      <div className="bookmark-add-button" onClick={() => bookmarkAdd()}>
-        +
-      </div>
+
+      <div className="bookmark-wrapper">{currentBookMark?.id}</div>
     </Container>
   )
-}
-
-BookRoom.getInitialProps = async ({ query }) => {
-  let bookId = query.id
-
-  const book = await API.Book.fetchBook(Number(bookId))
-
-  const bookMarkPayload = await API.Bookmark.fetchAllBookmarks(Number(bookId))
-
-  return { book: book[0], bookmarkList: bookMarkPayload }
 }
 
 export default BookRoom
