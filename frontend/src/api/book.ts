7df7e5b3bec68c9654d5f50d540230b82cc9d6ca
이찬호 <0407chan@naver.com>
2021-03-02 00:57:52 +0900

    fix. api 함수 return 타입 추가

--- frontend/src/api/book.ts
+++ frontend/src/api/book.ts
@@ -3,21 +3,21 @@ import { BookAddType, BookType, FetchBookType } from '../types'
 import { isDev } from '../utils'
 
 const API_SERVER = isDev ? 'http://localhost:3010' : 'https://withbook.ml'
-export const fetchAllBooks = async () => {
+export const fetchAllBooks = async (): Promise<BookType[]> => {
   const res = await axios.get<BookType[]>(`${API_SERVER}/api/v1/book`)
   return res.data
 }
 
-export const fetchBook = async (id: number) => {
+export const fetchBook = async (id: number): Promise<BookType[]> => {
   const res = await axios.get<BookType[]>(`${API_SERVER}/api/v1/book/${id}`)
   return res.data
 }
 
-export const addBook = async (newBook: BookAddType) => {
+export const addBook = async (newBook: BookAddType): Promise<BookType[]> => {
   const res = await axios.post<BookType[]>(`${API_SERVER}/api/v1/book`, newBook)
   return res.data
 }
-export const deleteBook = async (id: number) => {
+export const deleteBook = async (id: number): Promise<number> => {
   const res = await axios.delete<number>(`${API_SERVER}/api/v1/book/${id}`)
 
   return res.data
--- frontend/src/api/bookmark.ts
+++ frontend/src/api/bookmark.ts
@@ -4,21 +4,25 @@ import { isDev } from '../utils'
 
 const API_SERVER = isDev ? 'http://localhost:3010' : 'https://withbook.ml'
 
-export const fetchAllBookmarks = async (bookId: number) => {
+export const fetchAllBookmarks = async (
+  bookId: number
+): Promise<BookmarkType[]> => {
   const res = await axios.get<BookmarkType[]>(
     `${API_SERVER}/api/v1/bookmark/all/${bookId}`
   )
   return res.data
 }
 
-export const fetchBookmark = async (id: number) => {
+export const fetchBookmark = async (id: number): Promise<BookmarkType> => {
   const res = await axios.get<BookmarkType>(
     `${API_SERVER}/api/v1/bookmark/${id}`
   )
   return res.data
 }
 
-export const addBookmark = async (newBookmark: BookmarkBodyType) => {
+export const addBookmark = async (
+  newBookmark: BookmarkBodyType
+): Promise<BookmarkType[]> => {
   const res = await axios.post<BookmarkType[]>(
     `${API_SERVER}/api/v1/bookmark`,
     newBookmark
@@ -26,14 +30,16 @@ export const addBookmark = async (newBookmark: BookmarkBodyType) => {
   return res.data
 }
 
-export const updateBookmark = async (newBookmark: BookmarkType) => {
+export const updateBookmark = async (
+  newBookmark: BookmarkType
+): Promise<BookmarkType> => {
   const res = await axios.patch<BookmarkType>(
     `${API_SERVER}/api/v1/bookmark`,
     newBookmark
   )
   return res.data
 }
-export const deleteBookmark = async (id: number) => {
+export const deleteBookmark = async (id: number): Promise<number> => {
   const res = await axios.delete<number>(`${API_SERVER}/api/v1/bookmark/${id}`)
 
   return res.data
