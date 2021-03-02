1c81c5752c22f24dc20bb41f0963ea5094fb9910
이찬호 <0407chan@naver.com>
2021-03-02 23:31:33 +0900

    viewBookMark 기본 정보 추가

--- frontend/src/components/viewBook/viewBookMark/ViewBookMark.tsx
+++ frontend/src/components/viewBook/viewBookMark/ViewBookMark.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useRecoilValue } from 'recoil'
 import styled from 'styled-components'
 import { currentBookMarkState } from '../../../recoil/book'
@@ -15,6 +15,26 @@ const Container = styled.div`
   -ms-overflow-style: none;
   scrollbar-width: none;
   border-left: 1px solid #393652;
+
+  .bookMark-info {
+    display: flex;
+    flex-direction: row;
+    color: #dfe2e8;
+    padding: 20px;
+    width: 100%;
+    height: fit-content;
+    align-items: center;
+    .bookMark-page {
+      display: flex;
+      padding: 10px;
+      font-size: 24px;
+    }
+    .bookMark-title {
+      display: flex;
+      padding: 10px;
+      font-size: 18px;
+    }
+  }
 `
 
 type Props = unknown
@@ -22,10 +42,28 @@ type Props = unknown
 // 코멘트 fetch 해와야한다
 const ViewBookMark: React.FC<Props> = ({}) => {
   const currentBookMark = useRecoilValue(currentBookMarkState)
+
+  // fetch Comment
+  const initComment = useRef(() => {})
+  initComment.current = () => {
+    console.log('코멘트 fetch')
+  }
+
+  useEffect(() => {
+    initComment.current()
+  }, [])
+
   if (currentBookMark === undefined) {
     return <></>
   }
-
-  return <Container>{currentBookMark?.title}</Container>
+  return (
+    <Container>
+      <div className="bookMark-info">
+        <div className="bookMark-page">{currentBookMark?.bookpage}</div>
+        <div className="bookMark-title">{currentBookMark?.title}</div>
+        <div className="bookMark-comment-wrapper"></div>
+      </div>
+    </Container>
+  )
 }
 export default ViewBookMark
