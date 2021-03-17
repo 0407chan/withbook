33905140fe124b0b0b59ad8069c2a6efd78149d8
이찬호 <0407chan@naver.com>
2021-03-17 23:12:17 +0900

    색깔 정리

--- frontend/src/config/day-night-mode.tsx
+++ frontend/src/config/day-night-mode.tsx
@@ -11,8 +11,13 @@ export const DAY_BOOK_BG_HOVER_COLOR = '#e1effe'
 export const NIGHT_BOOK_BG_HOVER_COLOR = '#ac8f4f'
 
 export const NIGHT = {
-  headerWithBookHoverColor: '#f0bb34'
+  headerWithBookHoverColor: '#f0bb34',
+  BackgroundColor: '#232132',
+  FontColor: '#f0f0f0'
 }
+
 export const DAY = {
-  headerWithBookHoverColor: '#2b92ff'
+  headerWithBookHoverColor: '#2b92ff',
+  BackgroundColor: '#f0f0f0',
+  FontColor: '#232132'
 }
