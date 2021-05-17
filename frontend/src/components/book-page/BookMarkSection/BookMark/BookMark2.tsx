11477e98d94b920fd238ff45b9d97eb393f41428
ksj0206 <dapsyche@daum.net>
2021-05-18 02:43:49 +0900

    WIT-18/bookmark/add bookmark hover effect

--- frontend/src/components/book-page/BookMarkSection/BookMark/BookMark2.tsx
+++ frontend/src/components/book-page/BookMarkSection/BookMark/BookMark2.tsx
@@ -4,24 +4,25 @@ import styled from 'styled-components'
 const ribbonWidth = 30
 
 const Container = styled.div`
-  .bookmark-wrapper2 {
-    margin: 10px 10px;
-    height: 198px;
-    width: 310px;
+  .bookmark {
+    padding: 10px 10px;
+    height: 213px;
+    width: 325px;
     position: relative;
+    cursor: pointer;
   }
-  .bookmark-front {
+  .front {
     height: 188px;
     width: 300px;
-    left: 5px;
-    top: 5px;
+    left: ${10 + 5}px; // border 기준 > padding + 모서리영역
+    top: ${10 + 5}px;
     position: absolute;
     background-color: #232132;
     box-shadow: 1px 1px 6px rgba(255, 255, 255, 0.1);
     z-index: 1;
   }
 
-  .bookmark-back {
+  .back {
     content: '';
     height: 188px;
     width: 300px;
@@ -51,7 +52,7 @@ const Container = styled.div`
     border-bottom: ${ribbonWidth / 2}px solid transparent;
   }
 
-  .bookmark-content {
+  .content {
     width: 100%;
     height: 100%;
     padding: 60px 20px 20px;
@@ -61,19 +62,28 @@ const Container = styled.div`
     color: #eeeeee;
     font-size: 17px;
   }
+
+  .bookmark:hover .front {
+    transform: translate(3px, 3px);
+    box-shadow: 5px 5px 15px rgba(255, 255, 255, 0.1);
+    transition: all 0.2s ease-in-out;
+  }
+  .bookmark:hover .back {
+    transform: translate(-2px, -2px);
+    box-shadow: -2px -2px 6px rgba(255, 255, 255, 0.5);
+    transition: all 0.2s ease;
+  }
 `
 
 const BookMark2: React.FC = () => {
   return (
     <Container>
-      <div className="bookmark-wrapper2">
-        <div className="bookmark-front">
+      <div className="bookmark">
+        <div className="front">
           <span className="ribbon"></span>
-          <div className="bookmark-content">
-            꽃이 진다고 그대를 잊은 적 없다.
-          </div>
+          <div className="content">꽃이 진다고 그대를 잊은 적 없다.</div>
         </div>
-        <div className="bookmark-back"></div>
+        <div className="back"></div>
       </div>
     </Container>
   )
